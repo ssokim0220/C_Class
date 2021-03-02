f7af4f8ca0edcdf0bf55117ed6347a627ef439b5
ssokim0220 <79901371+ssokim0220@users.noreply.github.com>
2021-03-02 17:31:53 +0900

    Add files via upload

---
+++ Week1/Test1.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+main()
+{
+/*    printf("Good Morning. ");
+//  printf("안녕하세요.  Hi");
+    printf("Hello WORLD!\n");
+    printf("%d\n", 1234);
+    printf("%d, %d \n\n", 10, 20);
+   // return 0;
+	printf("안녕하세여. \n");
+	printf("제 이름은 %s 입니다. \n", "김소연"); 
+	printf("나이는 %d살 입니다. \n", 32);
+	printf("제 전공은 재료공학 입니다. \n");
+	printf("앞으로 약 %d개월 동안 잘 부탁드립니다.\n\n", 5);
+	
+	printf("경고음 소리 발생 : \\a \a \n");
+	printf("Backspace : \\b \b \n");
+	printf("Form Feed : \\f \f \n");
+	printf("개행 : \\n \n");
+	printf("캐리지 리턴 : \\r \r\n");
+	printf("수평 탭 : \\t \t \n");
+	printf("수직 탭 : \\v \v \n");
+	printf("백슬래시 : \\\ \\ \n");
+	printf("작은 따옴표 : \\' \' \n");
+	printf("큰 따옴표 : \\\" \" \n\n");
+	
+	printf("%d%d\n", 123, 456);
+	printf("%-8d %-8d %d\n", 123, 456, 789);
+	printf("%8d %8d %8d\n\n", 123, 456, 789);
+	printf("실수의 출력 : %8.2f %10.3f", 3.14159265, 456.789123);
+	
+	
+	3+4;  //
+	return 0;
+*/
+    printf("data type별 byte 수(int) : %d byte\n", sizeof(int));
+	printf("data type별 byte 수(char) : %d byte\n", sizeof(char));
+	printf("data tayp별 byte 수(long) : %d byte\n", sizeof(long));
+	printf("data type별 byte 수(float) : %d byte\n", sizeof(float));
+	printf("data type별 byte 수(double) : %d byte\n\n\n\n", sizeof(double));
+
+	
+}
